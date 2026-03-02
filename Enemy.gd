1586a467214ff40f37317c53621cf6416ab81f60
JoaquinOre <zombiesspace12@gmail.com>
2026-03-02 14:32:24 -0300

    primera version

---
+++ Enemy.gd
@@ -0,0 +1,59 @@
+extends Node2D
+enum Pose { IZQUIERDA, ARRIBA, DERECHA }
+@onready var Att_izq = $ColorRect/Attack_izq
+@onready var Att_der = $ColorRect/Attack_der
+@onready var Att_arr = $ColorRect/Attack_arr
+@export var node_2d : Node2D
+var enemyAttack = false
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	ocultar_ataque()
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func esperar_para_atacar():
+	await get_tree().create_timer(0.7).timeout
+	print("preparando el ataque")
+	preparar_ataque()
+
+func preparar_ataque():
+	var direccion = [Pose.IZQUIERDA, Pose.ARRIBA, Pose.DERECHA].pick_random()
+	ocultar_ataque()
+	if direccion == Pose.IZQUIERDA:
+		Att_izq.visible = true
+	if direccion == Pose.DERECHA:
+		Att_der.visible = true
+	if direccion == Pose.ARRIBA:
+		Att_arr.visible = true
+	await get_tree().create_timer(0.7).timeout
+	evaluar_ataque(direccion)
+
+func evaluar_ataque(dir):
+	var blockeo = false
+	if dir == Pose.IZQUIERDA and node_2d.escudo_izq.visible:
+		blockeo = true
+	if dir == Pose.DERECHA and node_2d.escudo_der.visible:
+		blockeo = true
+	if dir == Pose.ARRIBA and node_2d.escudo_arr.visible:
+		blockeo = true
+	
+	if blockeo == true:
+		print("el jugador blockeó el ataque")
+	else:
+		node_2d.vida -= 20
+		print("¡Golpe acertado al jugador!")
+		print(node_2d.vida)
+	
+	ocultar_ataque()
+	if enemyAttack == true:
+		preparar_ataque()
+
+func ocultar_ataque():
+	Att_izq.visible = false
+	Att_der.visible = false
+	Att_arr.visible = false
---
+++ escena_main.gd
@@ -0,0 +1,20 @@
+extends Node2D
+@onready var jugador = $Node2D
+@onready var enemigo = $Node2D2
+
+
+
+
+func _ready() -> void:
+	turno_del_enemigo()
+
+
+func turno_del_enemigo():
+	enemigo.enemyAttack = true
+	enemigo.preparar_ataque()
+	await get_tree().create_timer(2.0).timeout
+	enemigo.enemyAttack = false
+	turno_del_jugador()
+
+func turno_del_jugador():
+	print("turno del jugador")
